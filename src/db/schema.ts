0fbb84bb0deab3d8caf6e2f285b1ea2c224424ca
Alex-B012 <rad15on2014@gmail.com>
2026-06-30 18:34:00 +0300

    One-to-one connection for Drizzle

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import {
   pgTable,
   varchar,
@@ -11,7 +12,41 @@ export const products = pgTable("products", {
   name: varchar("name", { length: 255 }).notNull(),
   price: integer("price").notNull(),
   stock: integer("stock").notNull(),
+  brandId: uuid("brand_id").references(() => brands.id),
   createdAt: timestamp("created_at").defaultNow(),
 });
 
-// 5:46:25
+export const productsRelations = relations(products, ({ one }) => ({
+  brand: one(brands, {
+    fields: [products.brandId],
+    references: [brands.id],
+  }),
+  details: one(productDetails),
+}));
+
+export const productDetails = pgTable("product_details", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  description: varchar("description", { length: 2000 }),
+  materials: varchar("materials", { length: 500 }),
+  productId: uuid("product_id")
+    .notNull()
+    .unique()
+    .references(() => products.id),
+});
+
+export const productDetailsRelations = relations(productDetails, ({ one }) => ({
+  product: one(products, {
+    fields: [productDetails.productId],
+    references: [products.id],
+  }),
+}));
+
+export const brands = pgTable("brands", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar("name", { length: 100 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export const brandsRelations = relations(brands, ({ many }) => ({
+  products: many(products),
+}));
